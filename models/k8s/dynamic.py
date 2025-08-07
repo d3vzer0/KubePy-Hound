1bd8a68205b7df56960d864b6eda9e740bea6e29
Joey Dreijer <jdreijer@specterops.io>
2025-08-07 10:29:54 -0700

    Added edges for dynamic resources

---
+++ models/k8s/dynamic.py
@@ -0,0 +1,87 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from models.entries import Node, NodeProperties, Edge, EdgePath
+from models import lookups
+from pydantic import Field
+
+
+VERB_TO_PERMISSION = {
+    "get": "CAN_GET",
+    "list": "CAN_LIST",
+    "watch": "CAN_WATCH",
+    "create": "CAN_CREATE",
+    "update": "CAN_UPDATE",
+    "patch": "CAN_PATCH",
+    "delete": "CAN_DELETE",
+    "deletecollection": "CAN_DELETE_COLLECTION",
+    "proxy": "CAN_PROXY",
+    "*": "CAN_ALL",
+}
+
+
+class Metadata(BaseModel):
+    name: str
+    uid: str
+    namespace: str
+    creation_timestamp: datetime | None = None
+    labels: dict = {}
+    annotations: dict = {}
+
+
+class SourceRole(BaseModel):
+    name: str
+    uid: str
+    permissions: list[str]
+
+
+class DynamicResource(BaseModel):
+    kind: str
+    role: SourceRole
+    metadata: Metadata
+
+
+class ExtendedProperties(NodeProperties):
+    model_config = ConfigDict(extra='allow')
+    kind: str
+    namespace: str
+
+
+class DynamicNode(Node):
+    properties: ExtendedProperties
+    source_role_uid: str = Field(exclude=True)
+    source_role_permissions: list[str] = Field(exclude=True)
+
+    @property
+    def _namespace_edge(self):
+        target_id = lookups.namespaces[self.properties.namespace]
+        start_path = EdgePath(value=self.id, match_by='id')
+        end_path = EdgePath(value=target_id, match_by='id')
+        edge = Edge(kind='BELONGS_TO', start=start_path, end=end_path)
+        return edge
+
+    @property
+    def _role_edge(self):
+        role_edges = []
+        for permission in self.source_role_permissions:
+            target_id = self.source_role_uid
+            start_path = EdgePath(value=self.id, match_by='id')
+            end_path = EdgePath(value=target_id, match_by='id')
+            mapped_permission = VERB_TO_PERMISSION[permission]
+            edge = Edge(kind=mapped_permission, start=start_path, end=end_path)
+            role_edges.append(edge)
+        return role_edges
+
+    @property
+    def edges(self):
+        return [*self._role_edge, self._namespace_edge]
+
+    @classmethod
+    def from_input(cls, **kwargs) -> "DynamicNode":
+        kube_resource = DynamicResource(**kwargs)
+        properties = ExtendedProperties(name=kube_resource.metadata.name,
+                                        displayname=kube_resource.metadata.name,
+                                        namespace=kube_resource.metadata.namespace,
+                                        **kube_resource.metadata.labels,
+                                        )
+        return cls(id=kube_resource.metadata.uid, kinds=[f"Kube{kube_resource.kind}"], properties=properties,
+                   source_role_uid=kube_resource.role.uid, source_role_permissions=kube_resource.role.permissions)
