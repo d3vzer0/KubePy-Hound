35bbd13dc56966d6befc9bd7107f530c5e78bf89
Joey Dreijer <jdreijer@specterops.io>
2025-09-29 20:13:27 +0200

    Add daemonset, replicaset, deployment and satefulset

---
+++ kubepyhound/models/k8s/replicaset.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel, field_validator, ConfigDict
+from datetime import datetime
+
+from kubepyhound.models.entries import Node, NodeProperties, Edge, EdgePath
+
+# from pydantic_core import PydanticUseDefault
+from kubepyhound.utils.guid import get_guid
+from kubepyhound.utils.guid import NodeTypes
+from kubepyhound.models.k8s.pod import Container
+
+
+class OwnerReferences(BaseModel):
+    api_version: str
+    controller: bool
+    kind: str
+    name: str
+    uid: str
+
+
+class Metadata(BaseModel):
+    name: str
+    uid: str
+    namespace: str
+    creation_timestamp: datetime
+    labels: dict | None = {}
+    owner_references: list[OwnerReferences] | None = None
+
+    @field_validator("labels", mode="before")
+    def set_default_if_none(cls, v):
+        return v if v is not None else {}
+
+
+class ReplicaSet(BaseModel):
+    kind: str | None = "ReplicaSet"
+    metadata: Metadata
+
+    @field_validator("kind", mode="before")
+    def set_default_if_none(cls, v):
+        return v if v is not None else "ReplicaSet"
+
+
+class ExtendedProperties(NodeProperties):
+    model_config = ConfigDict(extra="allow")
+
+
+class ReplicaSetNode(Node):
+    properties: ExtendedProperties
+
+    @classmethod
+    def from_input(cls, **kwargs) -> "ReplicaSetNode":
+        model = ReplicaSet(**kwargs)
+        properties = ExtendedProperties(
+            name=model.metadata.name,
+            displayname=model.metadata.name,
+            namespace=model.metadata.namespace,
+            uid=model.metadata.uid,
+        )
+        node = cls(kinds=["K8sReplicaSet"], properties=properties)
+        return node
